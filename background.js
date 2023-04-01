82e017892048dd82c0ef02ae3ab05fcb18085378
Kevin Long <kevin.uu.long@gmail.com>
2023-04-01 17:25:16 -0400

    Getting closer
    Now I think it should just be a matter of some minor bugfixes until I have a working prototype.

--- background.js
+++ background.js
@@ -1,13 +1,15 @@
-const config = {
-    chance: 0.50,
-    images: 20,
-    random: () => { return Math.floor(Math.random() * config.images) + 1 }
-}
-
 function demeter() {
+
+    const config = {
+        chance: 0.50,
+        images: 20,
+        random: () => { return Math.floor(Math.random() * config.images) + 1 }
+    }
+
     let images = document.querySelectorAll('image');
 
-    for (const image in images) {
+    for (const image of images) {
+        console.log(image);
         if (Math.random() < config.chance) {
             image.src = `./images/lizard_${config.random()}`;
             image.style.objectFit = 'cover';
@@ -17,6 +19,9 @@ function demeter() {
 }
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
+    console.log('Switched tab');
+    if (chrome.tabs.get(activeInfo.tabId).url?.startsWith("chrome://")) return undefined;
+    console.log("Valid tab");
     chrome.scripting.executeScript({
         target: { tabId: activeInfo.tabId },
         function: demeter
